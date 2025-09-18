899fb784cf2444a3d755003b4b1d64487391851c
maiki044 <mayquerodrigues04@gmail.com>
2025-09-17 23:39:28 -0300

    update

--- API/server.js
+++ API/server.js
@@ -1,17 +1,27 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/prisma'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
 const users = []
 
-app.post('/usuarios', (req, res) => {
-  users.push(req.body)
-  res.send('Usuário adicionado com sucesso!')
+app.post('/usuarios', async (req, res) => {
+
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age
+    }
+  })
+  res.status(201).json(req.body)
 })
 
 app.get('/usuarios', (req, res) => {
-  res.json(users)
+  res.status(200).json(users)
 })
 
-app.listen(3000) 
+app.listen(3000)
